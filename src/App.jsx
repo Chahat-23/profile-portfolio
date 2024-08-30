d2ee64884bb8534c7c6e900a43d0a28d8d3005aa
Chahat-23 <chahatshrivastava230620@gmail.com>
2024-08-31 01:41:08 +0530

    Step-1-Seggregated-Components-Initiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import profileImage from "./assets/profileImage.jpg";
 import "./App.css";
 import bgVideo from "./assets/bgVideo.mp4";
+import SocialProfileCard from "./components/SocialProfileCard/SocialProfileCard";
 
 function App() {
-  const handleButtonClick = (url) => {
-    window.open(url, "_blank");
-  };
 
   return (
     <div className="app-container">
@@ -15,27 +12,7 @@ function App() {
         Video not supported!
       </video>
 
-      <div className="social-profile-card">
-        <img src={profileImage} alt="profile-image" className="profile-image" />
-        <div className="h1">Chahat Shrivastava</div>
-        <div className="location">Bengaluru, India</div>
-        <p>Frontend developer & web3 enthusiast</p>
-
-        <div className="buttons">
-          <button onClick={() =>handleButtonClick("https://vercel.com/chahat-23s-projects")}>
-            Projects
-          </button>
-          <button onClick={() => handleButtonClick("https://github.com/Chahat-23")}>
-            GitHub
-          </button>
-          <button onClick={() => handleButtonClick("https://www.linkedin.com/in/chahat-shrivastava-a67006185/")}>
-            LinkedIn
-          </button>
-          <button onClick={() => handleButtonClick("https://x.com/Chahat23S")}>
-            Twitter
-          </button>
-        </div>
-      </div>
+      <SocialProfileCard/>
     </div>
   );
 }
---
+++ src/components/SocialProfileCard/SocialProfileCard.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./SocialProfileCard.css";
+import profileImage from "../../assets/profileImage.jpg";
+
+export default function SocialProfileCard() {
+    
+  const handleButtonClick = (url) => {
+    window.open(url, "_blank");
+  };
+
+  return (
+    <div className="social-profile-card">
+      <img src={profileImage} alt="profile-image" className="profile-image" />
+      <div className="h1">Chahat Shrivastava</div>
+      <div className="location">Bengaluru, India</div>
+      <p>Frontend developer & web3 enthusiast</p>
+
+      <div className="buttons">
+        <button
+          onClick={() =>
+            handleButtonClick("https://vercel.com/chahat-23s-projects")
+          }
+        >
+          Projects
+        </button>
+        <button
+          onClick={() => handleButtonClick("https://github.com/Chahat-23")}
+        >
+          GitHub
+        </button>
+        <button
+          onClick={() =>
+            handleButtonClick(
+              "https://www.linkedin.com/in/chahat-shrivastava-a67006185/"
+            )
+          }
+        >
+          LinkedIn
+        </button>
+        <button onClick={() => handleButtonClick("https://x.com/Chahat23S")}>
+          Twitter
+        </button>
+      </div>
+    </div>
+  );
+}
