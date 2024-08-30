0d789858b0b13b7d486e7bfe47c3d4f32a9681b1
Chahat-23 <chahatshrivastava230620@gmail.com>
2024-08-31 03:45:32 +0530

    1.5

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import "./App.css";
 import bgVideo from "./assets/bgVideo.mp4";
 import SocialProfileCard from "./components/SocialProfileCard/SocialProfileCard";
+import wave from "./assets/wave.svg";
 
 function App() {
 
   return (
     <div className="app-container">
+      <div className="greetings">
+        <div className="welcome">Welcome to my portfolio!</div>
+        <img src={wave} className="hello" />
+      </div>
       <video autoPlay loop muted className="bg-video">
         <source src={bgVideo} type="video/mp4" />
         Video not supported!
       </video>
 
-      <SocialProfileCard/>
+      <SocialProfileCard />
     </div>
   );
 }
